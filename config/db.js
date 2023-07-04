4924afc7ce569ceb8a81ba3622372690e6c63e9c
Saijal Sikka <134805805+SaijalSikka@users.noreply.github.com>
2023-07-04 17:49:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 const connectDB=async() =>{
     try{
-        const conn=await mongoose.connect("mongodb://127.0.0.1/ecomUsersDB", { useNewUrlParser: true, useUnifiedTopology: true });
+        const mongoURL=process.env.MONDODB_URL;
+        const conn=await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Connected to the database");
     } catch(error){
         console.log("Database connection error:", error);
